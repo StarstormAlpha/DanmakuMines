4634575901b02420fa62893bb03c4430dceb9fbb
StarstormAlpha <StarstormAlpha>
2022-07-10 20:32:33 +0200

    delete old game

--- src/util/danmakuMinesConstants.ts
+++ src/util/danmakuMinesConstants.ts
@@ -1,23 +1,7 @@
-import { Color } from 'excalibur';
-
 /**
  * the dimensions: width and height of the scene
  */
 export const GAME_DIMENSIONS = {
     gameWidth: 900,
     gameHeight: 675,
-};
-
-/**
- * the brick dimensions
- */
-export const BRICK_CONSTANTS = {
-    padding: 20,
-    xOffset: 65,
-    yOffset: 20,
-    columns: 5,
-    rows: 3,
-    brickHeight: 30,
-    // brickWidth is calculated with the game dimensions and the padding
-    brickColors: [Color.Violet, Color.Orange, Color.Yellow],
 };
